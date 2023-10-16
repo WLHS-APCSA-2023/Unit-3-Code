d3f3767d08c7a657caf91f4a42a33c8b86947a84
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-16 14:57:08 -0700

    Update Bill.java
    added tip and switch case

--- Lessons3and4/Bill.java
+++ Lessons3and4/Bill.java
@@ -7,6 +7,38 @@ public class Bill {
         totalCustomers = numCustomers;
     }
 
+    public void addTip() {
+
+        if (totalCustomers >= 8) {
+            costOfMeal *= 1.25;
+        } else if (totalCustomers >= 4) {
+            costOfMeal *= 1.2;
+        }
+    }
+
+    public void addTipTwo() {
+        if(totalCustomers >= 8) {
+            costOfMeal *= 1.25;
+            return;
+            }
+            if(totalCustomers >= 4) costOfMeal *= 1.20;
+    }
+
+    public void tipSwitch(){
+        switch (totalCustomers){
+            case 10 , 9 , 8:
+                costOfMeal *= 1.25;
+                break;
+            case 7:
+            case 6:
+            case 5:
+                costOfMeal *= 1.2;
+                break;
+            default:
+                costOfMeal *= 1.05;
+        }
+    }
+
     public String toString() {
         return "Your bill is $" + costOfMeal;
     }
