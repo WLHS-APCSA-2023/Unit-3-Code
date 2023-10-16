0abfdf410a01e75a0ee53ca2fa4dfabae323d3ad
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-16 13:56:59 -0700

    Update Bill.java

--- Lessons3and4/Bill.java
+++ Lessons3and4/Bill.java
@@ -1,14 +1,13 @@
-public class Bill
-{
-	private double costOfMeal;
-	private int totalCustomers;
+public class Bill {
+    private double costOfMeal;
+    private int totalCustomers;
 
-	public Bill(double totalBill, int numCustomers)
-	{
-		costOfMeal = totalBill;
-		totalCustomers = numCustomers;
-	}
+    public Bill(double totalBill, int numCustomers) {
+        costOfMeal = totalBill;
+        totalCustomers = numCustomers;
+    }
 
-	public String toString(){
-		return "Your bill is $"+costOfMeal;
+    public String toString() {
+        return "Your bill is $" + costOfMeal;
+    }
 }
