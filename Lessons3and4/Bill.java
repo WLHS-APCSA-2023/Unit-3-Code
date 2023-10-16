afa2382164d37cefd5e786f5fd0d58bef91fd0e6
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-16 12:09:25 -0700

    Create Bill.java

---
+++ Lessons3and4/Bill.java
@@ -0,0 +1,11 @@
+public class Bill
+{
+	private double costOfMeal;
+	private int totalCustomers;
+
+	public Bill(double totalBill, int numCustomers)
+	{
+		costOfMeal = totalBill;
+		totalCustomers = numCustomers;
+	}
+}
