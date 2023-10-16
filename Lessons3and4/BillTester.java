1058d99fca4f999477e174a7f103b134956e321e
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-16 14:57:41 -0700

    Update BillTester.java

--- Lessons3and4/BillTester.java
+++ Lessons3and4/BillTester.java
@@ -1,6 +1,14 @@
 public class BillTester{
-  public static void main(String[] args){
-    Bill myBill = new Bill(47.64,3);
-  }
+    public static void main(String[] args){
+      Bill myBill = new Bill(100.00,9);
+      Bill myBillTwo = new Bill(100.00,6);
+      System.out.println(myBill);
+      myBill.tipSwitch();
+      System.out.println(myBill);
 
-}
+      System.out.println(myBillTwo);
+      myBillTwo.tipSwitch();
+      System.out.println(myBillTwo);
+    }
+  
+  }
