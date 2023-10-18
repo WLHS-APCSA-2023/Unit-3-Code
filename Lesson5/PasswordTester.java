c43bae3dbfd15ca9d56f99370959a191a73e7315
smithbwlhs <88860661+smithbwlhs@users.noreply.github.com>
2023-10-17 19:17:30 -0700

    Create PasswordTester.java

---
+++ Lesson5/PasswordTester.java
@@ -0,0 +1,7 @@
+public class PasswordTester {
+    public static void main(String[] args) {
+        //create and test 4 passwords, 2 that will fail and
+        //2 that will pass as strong passwords
+
+    }
+}
